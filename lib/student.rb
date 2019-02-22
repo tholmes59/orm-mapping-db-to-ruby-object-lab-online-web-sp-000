d949a9472cc97b69149f4f2e028c72509efffaaf
Tom Holmes <tholmes59@gmail.com>
2019-02-22 23:40:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,7 +88,17 @@ class Student
   end 
   
   def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10 
+      DESC 
+      LIMIT 1 
+    SQL
     
+    DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+    end
   end 
   
   def self.all_students_in_grade_X
