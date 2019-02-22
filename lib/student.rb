7c4037de2fdc66b9122aa8e979d8dcc5696eb514
Tom Holmes <tholmes59@gmail.com>
2019-02-22 23:53:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -104,8 +104,8 @@ class Student
       LIMIT 1
     SQL
     
-    DB[:conn].execute(sql).map do |row|
-    self.new_from_db(row)
+    DB[:conn].execute(sql)
+    self.new_from_db
     end
   end 
   
