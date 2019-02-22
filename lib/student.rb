ee46b9a00a18f14bb0ac60a2f6603856585a875f
Tom Holmes <tholmes59@gmail.com>
2019-02-22 23:37:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,15 @@ class Student
   end
   
   def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9 
+    SQL
     
+    DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+    end
   end 
   
   def self.students_below_12th_grade
@@ -75,7 +83,7 @@ class Student
     
   end 
   
-  def self 
+  def self.all_students_in_grade_X
     
   end 
   
